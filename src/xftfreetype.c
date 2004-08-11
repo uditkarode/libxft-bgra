14c188e8b0a0d88f97bcde21c8e1b1da8541bdf8
Kevin E Martin <kem@kem.org>
2004-08-11 23:37:34 +0000

    Called strlen with (f->file) without checking for NULL (which happens when     directly using FT_Face objects) (Bug #1047, Keith Packard).

--- src/xftfreetype.c
+++ src/xftfreetype.c
@@ -289,7 +289,8 @@ _XftReleaseFile (XftFtFile *f)
 	if (f->face)
 	    FT_Done_Face (f->face);
     }
-    XftMemFree (XFT_MEM_FILE, sizeof (XftFtFile) + strlen (f->file) + 1);
+    XftMemFree (XFT_MEM_FILE,
+		sizeof (XftFtFile) + f->file ? strlen (f->file) + 1 : 0);
     free (f);
 }
 
